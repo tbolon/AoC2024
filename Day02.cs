fa387e7645cfaf6c431f14e0f5e205caffe90e22
Thomas Bolon <tbolon@mb3m.fr>
2024-12-03 14:54:27 +0100

    dev

--- Day02.cs
+++ Day02.cs
@@ -5,36 +5,29 @@
         var safeCount = Input.GetLines(2)
             .Select(l => l.Split(' ').Select(x => int.Parse(x)).ToArray())
             .Count(r => IsSafe(r));
-        //.Count(r => IsSafe_Part2(r));
 
         WriteLine(safeCount);
+    }
 
-        static bool IsSafe(int[] rapport)
+    static bool IsSafe(int[] rapport)
+    {
+        if (ComputeIsSafeDiff(rapport))
+            return true;
+
+        // on brute force les combinaisons possibles
+        for (int i = 0; i < rapport.Length; i++)
         {
-            var diff = ComputeDiff(rapport);
-            if (IsSafeDiff(diff))
-            {
+            var rapport2 = rapport.Where((r, x) => x != i).ToArray();
+            if (ComputeIsSafeDiff(rapport2))
                 return true;
-            }
-
-            // on brute force les combinaisons possibles
-            for (int i = 0; i < rapport.Length; i++)
-            {
-                var rapport2 = rapport.Where((r, x) => x != i).ToArray();
-                var diff2 = ComputeDiff(rapport2);
-                if (IsSafeDiff(diff2))
-                    return true;
-            }
-
-            return false;
         }
 
-        static bool IsSafeDiff(int[] diffs)
-        {
-            return (diffs.All(i => i > 0) || diffs.All(i => i < 0)) && diffs.All(i => Math.Abs(i) >= 1 && Math.Abs(i) <= 3);
-        }
+        return false;
+    }
 
-        static int[] ComputeDiff(IEnumerable<int> rapport) => rapport.SkipLast(1).Select((x, i) => rapport.ElementAt(i + 1) - x).ToArray();
-
+    static bool ComputeIsSafeDiff(IEnumerable<int> rapport)
+    {
+        var diffs = rapport.SkipLast(1).Select((x, i) => rapport.ElementAt(i + 1) - x).ToArray();
+        return (diffs.All(i => i > 0) || diffs.All(i => i < 0)) && diffs.All(i => Math.Abs(i) >= 1 && Math.Abs(i) <= 3);
     }
 }
