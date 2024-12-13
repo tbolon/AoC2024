63f1ae238b485d4357d19a2aace43c1f25bd9aeb
Thomas Bolon <tbolon@mb3m.fr>
2024-12-13 15:48:11 +0100

    day12

--- AoC.2024/Day12.cs
+++ AoC.2024/Day12.cs
@@ -1,7 +1,105 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
 namespace AoC2024;
 
 public static class Day12
 {
+    public static long Solve()
+    {
+        var grid = Input.GetLines(sample: false).AsGridOfChars();
+
+        var gardens = new List<Garden>();
+
+        // remplissage jardins
+        foreach (var cell in grid)
+        {
+            if (!gardens.Any(g => g.Contains(cell.Point)))
+            {
+                var garden = new Garden(cell.Value, cell.Point);
+                gardens.Add(garden);
+                garden.Fill(grid);
+                //WriteLine($"{garden.Letter} Area = {garden.Area} ; Perimeter = {garden.Perimeter} ; Price = {garden.Price}");
+            }
+        }
+
+        foreach (var garden in gardens)
+        {
+            HashSet<Point> horizSegments = [];
+            HashSet<Point> vertSegments = [];
+
+            foreach (var point in garden.Points)
+            {
+                if (!garden.Contains(point.Up))
+                    horizSegments.Add(point);
+                if (!garden.Contains(point.Left))
+                    vertSegments.Add(point);
+                if (!garden.Contains(point.Down))
+                    horizSegments.Add(point.Down);
+                if (!garden.Contains(point.Right))
+                    vertSegments.Add(point.Right);
+            }
+
+            var segCount = 0;
+            Point previous = Point.Empty;
+            bool gardenIsDown = false;
+            foreach (var point in horizSegments.OrderBy(s => s.Y).ThenBy(s => s.X))
+            {
+                if (previous.Right != point)
+                {
+                    // different fence
+                    segCount++;
+                    gardenIsDown = garden.Contains(point);
+                    Assert(gardenIsDown || garden.Contains(point.Up));
+                }
+                else
+                {
+                    // same fence, check if the garden is on the same side
+                    var thisGardenIsDown = garden.Contains(point);
+                    if (gardenIsDown != thisGardenIsDown)
+                    {
+                        segCount++;
+                        gardenIsDown = thisGardenIsDown;
+                    }
+                }
+
+                previous = point;
+            }
+
+            previous = Point.Empty;
+            bool gardenIsRight = false;
+            foreach (var point in vertSegments.OrderBy(s => s.X).ThenBy(s => s.Y))
+            {
+                if (previous.Down != point)
+                {
+                    // different fence
+                    segCount++;
+                    gardenIsRight = garden.Contains(point);
+                    Assert(gardenIsRight || garden.Contains(point.Left));
+                }
+                else
+                {
+                    // same fence, check if the garden is on the same side
+                    var thisGardenIsRight = garden.Contains(point);
+                    if (gardenIsRight != thisGardenIsRight)
+                    {
+                        segCount++;
+                        gardenIsRight = thisGardenIsRight;
+                    }
+                }
+
+                previous = point;
+            }
+
+
+            garden.FenceCount = segCount;
+
+            WriteLine($"{garden.Letter} Area = {garden.Area} ; Fance Count = {garden.FenceCount} ; Price = {garden.FenceCount * garden.Area}");
+        }
+
+        return gardens.Sum(g => g.FenceCount * g.Area);
+    }
+
     public static long Solve_Part1()
     {
         var grid = Input.GetLines(sample: false).AsGridOfChars();
@@ -24,6 +122,34 @@ public static class Day12
         return gardens.Sum(g => g.Price);
     }
 
+    [DebuggerDisplay("[{P1.X},{P1.Y}];[{P2.X},{P2.Y}]")]
+    public readonly struct Line(Point p1, Point p2)
+    {
+        public Point P1 { get; } = p1;
+
+        public Point P2 { get; } = p2;
+
+        public bool IsVertical => P1.X == P2.X;
+
+        public bool IsHorizontal => P1.Y == P2.Y;
+
+        public double Length => IsVertical
+            ? Math.Abs(P2.Y - P1.Y)
+            : IsHorizontal
+            ? Math.Abs(P2.X - P1.X)
+            : Math.Sqrt(Math.Pow(P2.Y - P1.Y, 2) + Math.Pow(P2.X - P1.X, 2));
+
+
+        public override int GetHashCode() => P1.GetHashCode() ^ P2.GetHashCode();
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (!(obj is Line l2)) return false;
+
+            return (P1 == l2.P1 && P2 == l2.P2) || (P1 == l2.P2 && P2 == l2.P1);
+        }
+    }
+
     class Garden
     {
         private readonly HashSet<Point> points = new();
@@ -36,11 +162,15 @@ public static class Day12
             this.origin = origin;
         }
 
+        public IEnumerable<Point> Points => points;
+
         public char Letter { get; }
 
         public int Area => points.Count;
 
         public int Perimeter { get; private set; }
+
+        public int FenceCount { get; set; }
 
         public int Price => Area * Perimeter;
 
