fd0055aa54d8d7c80a407061a0939f202c4da958
Thomas BOLON <tbolon@mb3m.fr>
2024-12-15 10:49:31 +0100

    passage algo DBscan

--- AoC.2024/Day14.cs
+++ AoC.2024/Day14.cs
@@ -33,9 +33,19 @@ public static class Day14
 
         var count = robots.Count;
         var maxInCount = 0;
+        int maxClusters = 0;
 
         for (int i = 0; i < int.MaxValue; i++)
         {
+            var c = Dbscan.Dbscan.CalculateClusters(robots, epsilon: 15.0, minimumPointsPerCluster: 250);
+            if (c.Clusters.Count != 0)
+            {
+                WriteGridClusters(robots, c);
+                WriteLine($"Clusters = {c.Clusters.Count} ; i = {i}");
+                Console.ReadKey();
+                maxClusters = c.Clusters.Count;
+            }
+
             foreach (var robot in robots)
             {
                 robot.Move();
@@ -43,7 +53,7 @@ public static class Day14
 
             //WriteGrid(robots);
             //Console.ReadKey();
-
+            /*
             var countIn = robots.Count(r => r.IsInChristmasTree());
 
             if (countIn > maxInCount)
@@ -56,18 +66,50 @@ public static class Day14
                     Console.ReadKey();
                 }
 
-            }
+            }*/
 
             //if (robots.All(r => r.IsInChristmasTree()))
             //    return i;
 
-            //if (i % 10000 == 0)
-            //{
-            //    AnsiConsole.WriteLine(i);
-            //}
+            if (i % 100 == 0)
+            {
+                WriteLine(i);
+            }
         }
 
         return -1;
+    }
+
+    private static void WriteGridClusters(List<Robot> robots, Dbscan.ClusterSet<Robot> clusters)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                var p = new Point(x, y);
+                var pc = clusters.Clusters.Count(c => c.Objects.Any(r => r.Position == p));
+                var locs = robots.Count(r => r.Position == p);
+
+                var isIn = pc > 0;
+                var color = isIn ? ConsoleColor.Green : ConsoleColor.Red;
+                if (isIn)
+                {
+                    var countChar = (char)(pc > 9 ? '+' : '0' + pc);
+                    Write(countChar, color);
+                }
+                else if (locs > 0)
+                {
+                    Write('x', color);
+
+                }
+                else
+                {
+                    Write('.', ConsoleColor.Gray);
+                }
+            }
+
+            WriteLine();
+        }
     }
 
     private static void WriteGrid(List<Robot> robots)
@@ -170,13 +212,16 @@ public static class Day14
         return x >= minX && x <= maxX;
     }
 
-    record Robot(Point Position, Point Speed)
+    record Robot(Point Position, Point Speed) : Dbscan.IPointData
     {
         public Point Position { get; private set; } = Position;
+
+        public Dbscan.Point Point { get; private set; } = new Dbscan.Point(Position.X, Position.Y);
 
         public void Move()
         {
             Position = Modulo(Position.Add(Speed));
+            Point = new Dbscan.Point(Position.X, Position.Y);
         }
 
         public bool IsInChristmasTree() => Day14.IsInChristmasTree(Position);
